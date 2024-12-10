d7313ea07abe50f8fbf33e9e69798177b43fa072
Tomassaldana <tomassaldalegui@gmail.com>
2024-12-10 15:45:21 -0500

    clase cleinte

---
+++ Cliente.java
@@ -0,0 +1,28 @@
+package restaurante;
+import java.util.HashMap;
+import java.util.Map;
+/**
+ * Simulacion restaurante clase cliente con metodos:
+ * menu, carrito de compras y pagar
+ * @author Tomas Saldaña
+ */
+
+public class Cliente {
+    
+    public String[] menu = new String[3];
+    public int precio;
+    
+    public menu String[];
+    
+            
+    public Cuenta(int numeroCuenta, double saldo){ //Constructor
+        this.numeroCuenta = numeroCuenta;
+        this.Saldo = saldo;
+        
+        public void pagar(double precio) {
+        double impuesto = calcularImpuesto(monto);  
+        double saldoFinal = saldo - precio;
+    }
+    
+    
+}
