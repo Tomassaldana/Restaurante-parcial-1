189f5b016f106da073596ad380a6575f0d0f10ae
Tomassaldana <tomassaldalegui@gmail.com>
2024-12-10 15:45:45 -0500

    Clase cuenta

---
+++ cuenta.java
@@ -0,0 +1,30 @@
+
+package restaurante;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class cuenta {
+    private String nombre;
+    private int identificacion;
+    private Cuenta cuenta;
+    
+    public Cliente(String nombre, int identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        
+        public Cliente(String nombre, int identificacion) {
+        this.nombre = nombre;
+        this.identificacion = identificacion;
+        
+    }
+
+    public void setCuenta(Cuenta cuenta) {
+        this.cuenta = cuenta;
+    }
+
+    public Cuenta getCuenta() {
+        return cuenta;
+    }
+}
